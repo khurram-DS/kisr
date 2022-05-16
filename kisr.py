52f7f4d8d3b94a469b9de7db73052abeb7f3273b
khurram shakoor <73300634+khurram-DS@users.noreply.github.com>
2022-05-16 20:25:42 +0530

    Update kisr.py

--- kisr.py
+++ kisr.py
@@ -45,9 +45,9 @@ def main():
             diff1=df["The total value of the company's investments in 2020"]-df["The total value of the company's investments in 2019"]
             b1= diff1 < -2 
             b2= diff1 > 2
-        
+            b4= diff1 == 3
             df1["The total value of the company's investments in 2019"]= np.where(b1, 'background-color: {}'.format(r), df1["The total value of the company's investments in 2019"])
-    
+            df1["The total value of the company's investments in 2019"]=np.where(b4, df1["The total value of the company's investments in 2019"]+1 , df1["The total value of the company's investments in 2019"])
             df1["The total value of the company's investments in 2019"]= np.where(b2, 'background-color: {}'.format(r), df1["The total value of the company's investments in 2019"])
     
     
@@ -56,18 +56,7 @@ def main():
     
             
             return df1
-        da=df.style.apply(highlight_greater, axis =None)
-        diff1=da["The total value of the company's investments in 2020"]-da["The total value of the company's investments in 2019"]
-        b4= diff1==3
-        da["The total value of the company's investments in 2019"]=np.where(b4, da["The total value of the company's investments in 2019"]+1 , da["The total value of the company's investments in 2019"])
-        
-        
-        
-        
-        
-        
-        
-        
+        da=df.style.apply(highlight_greater, axis =None) 
         
         st.dataframe(da)
         
