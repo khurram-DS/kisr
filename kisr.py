38ee0f66197db89fed3797f4ae806f21f8fd42b3
khurram shakoor <73300634+khurram-DS@users.noreply.github.com>
2022-05-16 20:29:38 +0530

    Update kisr.py

--- kisr.py
+++ kisr.py
@@ -56,7 +56,7 @@ def main():
     
             
             return df1
-        da=df.style.apply(highlight_greater, axis =None) 
+        da=df1.style.apply(highlight_greater, axis =None) 
         
         st.dataframe(da)
         
