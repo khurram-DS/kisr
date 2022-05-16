59998f917b9e433a2b68acf85ebf497ec2efa5ea
khurram shakoor <73300634+khurram-DS@users.noreply.github.com>
2022-05-16 20:32:18 +0530

    Update kisr.py

--- kisr.py
+++ kisr.py
@@ -47,7 +47,7 @@ def main():
             b2= diff1 > 2
             b4= diff1 == 3
             df1["The total value of the company's investments in 2019"]= np.where(b1, 'background-color: {}'.format(r), df1["The total value of the company's investments in 2019"])
-            df["The total value of the company's investments in 2019"]=np.where(b4, df["The total value of the company's investments in 2019"]+1 , df["The total value of the company's investments in 2019"])
+            #df["The total value of the company's investments in 2019"]=np.where(b4, df["The total value of the company's investments in 2019"]+1 , df["The total value of the company's investments in 2019"])
             df1["The total value of the company's investments in 2019"]= np.where(b2, 'background-color: {}'.format(r), df1["The total value of the company's investments in 2019"])
     
     
